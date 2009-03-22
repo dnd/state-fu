4ee41ed129859e019fa10e3c71486e519765dd7d
David Lee <dml@shifter.(none)>
2009-03-22 21:34:40 +1100

    adding monkeypatch for ruby 1.8.x symbol.<=>

--- lib/zen/core_ext.rb
+++ lib/zen/core_ext.rb
@@ -2,6 +2,16 @@ require 'rubygems'
 # TODO - factor this out
 require 'active_support'
 
+# for ruby 1.8.x
+class Symbol
+  unless instance_methods.include?(:'<=>')
+    puts "monkeypatching symbol"
+    def <=> other
+      self.to_s <=> other.to_s
+    end
+  end
+end
+
 if Object.const_get("ActiveSupport").nil?
   # patch in what we need
 end
