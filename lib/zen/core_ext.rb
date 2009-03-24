3619be03cee932acca177866c36880d2d81da2e2
David Lee <david@davelee.com.au>
2009-03-24 18:25:58 +1100

    fixing active_support inclusions .. sigh

--- lib/zen/core_ext.rb
+++ lib/zen/core_ext.rb
@@ -1,20 +1,23 @@
 require 'rubygems'
 
-# todo : remove this once its exclusion no longer breaks anything on any platforms
-require 'activesupport'
+unless Object.const_defined?('ActiveSupport')
 
-require 'active_support/core_ext/array'
-require 'active_support/core_ext/blank'
-require 'active_support/core_ext/class'
-require 'active_support/core_ext/hash'
-# require 'active_support/core_ext/string'
+  require 'active_support/core_ext/array'
+  require 'active_support/core_ext/blank'
+  require 'active_support/core_ext/class'
+  require 'active_support/core_ext/module'
+  require 'active_support/core_ext/hash/keys'
+
+  class Hash #:nodoc:
+    include ActiveSupport::CoreExtensions::Hash::Keys
+  end
+end
 
 class Symbol
-  unless instance_methods.include?(:'<=>')
+  unless instance_methods.include?(:'<=>2')
     # Logger.log ..
     def <=> other
       self.to_s <=> other.to_s
     end
   end
 end
-
