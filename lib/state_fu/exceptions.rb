6f2de8fb0434efa8094d2dd64ed871874de6e014
David Lee <david@davelee.com.au>
2009-04-15 16:06:15 +1000

    added units for exceptions

--- lib/state_fu/exceptions.rb
+++ lib/state_fu/exceptions.rb
@@ -1,28 +1,30 @@
 module StateFu
 
   class Exception < ::Exception
-    attr_reader :binding
+    attr_reader :binding, :options
   end
 
   class TransitionHalted < Exception
     attr_reader :transition
     DEFAULT_MESSAGE = "The transition was halted"
-    def initialize( transition, message=DEFAULT_MESSAGE )
+    def initialize( transition, message=DEFAULT_MESSAGE, options={})
       @transition = transition
-      @message    = message
+      @options    = options
+      super( message )
     end
   end
 
   class InvalidTransition < Exception
-    attr_reader :binding, :origin, :target, :args
+    attr_reader :binding, :origin, :target, :event, :args
     DEFAULT_MESSAGE = "An invalid transition was attempted"
 
     def initialize( binding, event, origin, target, message=DEFAULT_MESSAGE, options={})
       @binding = binding
+      @event   = event
       @origin  = origin
       @target  = target
-      @message = message
       @options = options
+      super( message )
     end
   end
 
