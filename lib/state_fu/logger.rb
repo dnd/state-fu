0b56eab4994a2a610f71ef0b10b7eb1e2623af42
David Lee <david@davelee.com.au>
2009-04-15 16:10:58 +1000

    set StateFu to use RAILS_DEFAULT_LOGGER if defined

--- lib/state_fu/logger.rb
+++ lib/state_fu/logger.rb
@@ -1,6 +1,10 @@
 require 'logger'
 
 module StateFu
-  Logger       = ::Logger.new( STDOUT )
-  Logger.level = ::Logger.const_get( (ENV["ZEN_LOGLEVEL"] || 'WARN').upcase )
+  if Object.const_defined?( "RAILS_DEFAULT_LOGGER" )
+    Logger       = RAILS_DEFAULT_LOGGER
+  else
+    Logger       = ::Logger.new( STDOUT )
+    Logger.level = ::Logger.const_get( (ENV["ZEN_LOGLEVEL"] || 'WARN').upcase )
+  end
 end
