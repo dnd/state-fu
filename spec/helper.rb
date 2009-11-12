1fd646f981fe283b4aacc5dd54a864d247740205
davidlee <david@davelee.com.au>
2009-11-12 16:54:09 +1100

    Bugfix for stupid message when ruby double (rr) is not installed

--- spec/helper.rb
+++ spec/helper.rb
@@ -4,7 +4,7 @@ require File.join(File.dirname(__FILE__),'spec_helper')
 begin
   require 'rr'
 rescue LoadError => e
-  STDERR.puts "The '#{gem_name}' gem is required to run StateFu's specs. Please install it by running (as root):\ngem install #{gem_name}\n\n"
+  STDERR.puts "The 'rr' gem is required to run StateFu's specs. Please install it by running (as root):\n sudo gem install rr\n\n"
   exit 1;
 end
 
